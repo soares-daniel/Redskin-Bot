5596290f812d9843a1399229019ba16d97580f74
soares-daniel <daniel.soares.001@student.uni.lu>
2023-07-12 12:11:22 +0200

    better handling of api downtime, update calendar

--- bot/authorization/backend.py
+++ bot/authorization/backend.py
@@ -1,3 +1,6 @@
+import asyncio
+from aiohttp.client_exceptions import ClientConnectorError
+
 from models.schemas.user import UserInResponse, UserInLogin, UserWithToken
 from settings import API_URL, USERNAME, PASSWORD
 
@@ -6,19 +9,31 @@ AUTH_URL = f"{API_URL}/authorization/login"
 
 async def login(get_http_client, set_auth_user) -> None:
     """Login to API"""
-    http_client = get_http_client()()
-    login_user = UserInLogin(username=USERNAME, password=PASSWORD)
-    user_response = await http_client.post(AUTH_URL, login_user.dict())
-    auth_user = None
-    authorized_user = user_response.get('authorizedUser')
-    if authorized_user:
-        auth_user = UserInResponse(
-                    id=user_response.get("id"),
-                    authorized_user=UserWithToken(
-                        token=authorized_user.get('token'),
-                        username=authorized_user.get('username'),
-                        created_at=authorized_user.get('createdAt'),
-                        updated_at=authorized_user.get('updatedAt'),
-                    ),
-                )
-    set_auth_user(auth_user)
+    print("SETTING UP BACKEND CONNECTION...")
+
+    for attempt in range(3):
+        try:
+            http_client = get_http_client()()
+            login_user = UserInLogin(username=USERNAME, password=PASSWORD)
+            user_response = await http_client.post(AUTH_URL, login_user.dict())
+            auth_user = None
+            authorized_user = user_response.get('authorizedUser')
+            if authorized_user:
+                auth_user = UserInResponse(
+                            id=user_response.get("id"),
+                            authorized_user=UserWithToken(
+                                token=authorized_user.get('token'),
+                                username=authorized_user.get('username'),
+                                created_at=authorized_user.get('createdAt'),
+                                updated_at=authorized_user.get('updatedAt'),
+                            ),
+                        )
+            set_auth_user(auth_user)
+            print("AUTHORIZATION SUCCESSFUL!")
+            break
+        except ClientConnectorError as e:
+            print(f"CONNECTION UNSUCCESSFUL, API MAY BE DOWN! (Attempt {attempt+1}/3)")
+            if attempt < 2:  # If it's not the last attempt, wait a bit before trying again
+                await asyncio.sleep(5)
+            else:
+                raise ConnectionRefusedError("FAILED TO CONNECT TO API, SHUTTING DOWN...")
--- bot/bot.py
+++ bot/bot.py
@@ -1,3 +1,4 @@
+import json
 import logging
 from logging import handlers
 
@@ -6,9 +7,22 @@ from aiohttp import web
 from discord import Activity, ActivityType, Intents, ApplicationCommand
 from discord.ext import commands
 
+from bot.embeds.calendar import CalendarEmbed
+from bot.embeds.logging import (LogEmbed, EventLog, EventTypeLog, RoleLog,
+                                UserLog, UserRoleLog, PermissionLog)
+from models.schemas.event import EventInResponse
+from models.schemas.event_type import EventTypeInResponse
+from models.schemas.role import RoleInResponse
+from models.schemas.user import UserInResponse
+from models.schemas.user_role import UserRoleInAssign
+from models.schemas.role_event_type import RoleEventTypeInResponse
+
+
 from bot.requests.http_client import HttpClient
+from bot.requests.event import get_events
 from bot.authorization.backend import login
-from settings import SERVER_PORT, SERVER_HOST, NOTIFICATION_ENDPOINT, NOTIFICATION_CHANNEL_ID
+from settings import (SERVER_PORT, SERVER_HOST, NOTIFICATION_ENDPOINT, GUILD_ID,
+                      CATEGORY_NAME, CALENDAR_CHANNEL_NAME, NOTIFICATION_CHANNEL_NAME, COMMAND_CHANNEL_NAME)
 
 
 class PRDBot(commands.Bot):
@@ -19,6 +33,7 @@ class PRDBot(commands.Bot):
         activity = Activity(type=ActivityType.watching, name="Kalendar")
         intents = Intents.all()
         super().__init__(activity=activity, command_prefix="!", intents=intents)
+        
         # Logging
         logger = logging.getLogger(__name__)
         logger.setLevel(logging.DEBUG)
@@ -43,15 +58,29 @@ class PRDBot(commands.Bot):
         self.auth_user = None
         self.http_client: HttpClient = HttpClient(self.get_token, self.set_auth_user)
 
+        self.MAIN_GUILD = None
+
+        # Channels
+        self.CALENDAR_CHANNEL_ID = -1
+        self.NOTIFICATION_CHANNEL_ID = -1
+        self.COMMAND_CHANNEL_ID = -1
+
     async def on_ready(self) -> None:
         print("------")
         print("Les Peaux Rouges - Discord Bot")
         print(self.user.name)
         print("------")
-
-        # Get auth user
-        await login(self.get_http_client, self.set_auth_user)
+        await self.init_channels()
+        print("------")
+        try:
+            await login(self.get_http_client, self.set_auth_user)
+        except ConnectionRefusedError as e:
+            print(e)
+            await self.close()
+        print("------")
         await self.start_server()
+        print("------")
+        print("READY TO GO !")
 
     async def register_command(
             self, command: ApplicationCommand, force: bool = True, guild_ids: list[int] | None = None
@@ -69,7 +98,37 @@ class PRDBot(commands.Bot):
     def get_http_client(self):
         return lambda: self.http_client
 
+    async def init_channels(self):
+        print("INITIALIZING CHANNELS...")
+        guild = self.get_guild(int(GUILD_ID))
+        self.MAIN_GUILD = guild
+        category = discord.utils.get(guild.categories, name=CATEGORY_NAME)
+        if category is None:
+            category = await guild.create_category(CATEGORY_NAME)
+            print("CATEGORY CREATED")
+
+        calendar_channel = discord.utils.get(guild.channels, name=CALENDAR_CHANNEL_NAME.lower())
+        if calendar_channel is None:
+            calendar_channel = await guild.create_text_channel(CALENDAR_CHANNEL_NAME, category=category)
+            print("CALENDAR CHANNEL CREATED")
+
+        notification_channel = discord.utils.get(guild.channels, name=NOTIFICATION_CHANNEL_NAME.lower())
+        if notification_channel is None:
+            notification_channel = await guild.create_text_channel(NOTIFICATION_CHANNEL_NAME, category=category)
+            print("NOTIFICATION CHANNEL CREATED")
+
+        command_channel = discord.utils.get(guild.channels, name=COMMAND_CHANNEL_NAME.lower())
+        if command_channel is None:
+            command_channel = await guild.create_text_channel(COMMAND_CHANNEL_NAME, category=category)
+            print("COMMAND CHANNEL CREATED")
+
+        self.CALENDAR_CHANNEL_ID = calendar_channel.id
+        self.NOTIFICATION_CHANNEL_ID = notification_channel.id
+        self.COMMAND_CHANNEL_ID = command_channel.id
+        print("CHANNELS INITIALIZED")
+
     async def start_server(self):
+        print("STARTING SERVER...")
         app = web.Application()
         app.router.add_post(NOTIFICATION_ENDPOINT, self.on_event)
 
@@ -77,17 +136,68 @@ class PRDBot(commands.Bot):
         await runner.setup()
         site = web.TCPSite(runner=runner, host=SERVER_HOST, port=SERVER_PORT)
         await site.start()
-        print(f"Server started. Awaiting events on {NOTIFICATION_ENDPOINT}")
+        print(f"SERVER STARTED. AWAITING EVENTS ON: {NOTIFICATION_ENDPOINT}")
 
     async def on_event(self, request):
-        # Extract event data from the request
         response = await request.json()
         event_operation = response['event_operation']
-        event = response['event']
 
-        # Announce the event update in a specific channel
-        channel = await self.fetch_channel(NOTIFICATION_CHANNEL_ID)
-        if channel:
-            await channel.send(f"Event {event_operation}:\n{event}")
+        # Define a mapping from operation types to data models
+        model_map = {
+            "event": EventInResponse,
+            "event_type": EventTypeInResponse,
+            "role": RoleInResponse,
+            "user": UserInResponse,
+            "user_role": UserRoleInAssign,  # Remove and assign are the same model
+            "permission": RoleEventTypeInResponse
+        }
 
-        return web.Response()
+        # Extract the type of operation (e.g., 'event', 'user', etc.) from the operation string
+        operation_type = event_operation.split('_')[0]
+
+        # Use the map to get the appropriate model class
+        ModelClass = model_map.get(operation_type)
+
+        if ModelClass is not None:
+            # Deserialize the JSON data into a model object
+            event = ModelClass.parse_obj(json.loads(response['event']))
+
+            # Define a mapping from operation types to LogEmbed subclasses
+            log_class_map = {
+                "event": EventLog,
+                "event_type": EventTypeLog,
+                "role": RoleLog,
+                "user": UserLog,
+                "user_role": UserRoleLog,
+                "permission": PermissionLog
+            }
+
+            # Use the map to get the appropriate log class, and fall back to LogEmbed class if type not recognized
+            LogClass = log_class_map.get(operation_type, LogEmbed)
+
+            # Instantiate the log object and load the data
+            log = LogClass()
+            await log.load_data(event_operation.split('_')[1], event)
+
+            # Send the embed
+            channel = await self.fetch_channel(self.NOTIFICATION_CHANNEL_ID)
+            if channel:
+                await channel.send(embed=log)
+
+            # Update calendar if event
+            if operation_type == "event":
+                await self.create_calendar()
+
+        else:
+            print(f"Unsupported operation type: {operation_type}")
+
+            return web.Response()
+
+    async def create_calendar(self):
+        events = await get_events(self.http_client)
+        embed = CalendarEmbed(events=events).build()
+        channel = await self.fetch_channel(self.CALENDAR_CHANNEL_ID)
+        if channel is None:
+            return
+
+        await channel.send(embed=embed)  # type: ignore
