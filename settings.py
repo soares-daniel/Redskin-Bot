e3e2abcf37f86b4ede1d8a389ed401d47258a614
soares-daniel <daniel.soares.001@student.uni.lu>
2023-07-22 14:48:05 +0200

    removed unnecessary line

--- settings.py
+++ settings.py
@@ -6,7 +6,6 @@ from dotenv import load_dotenv
 
 DEBUG = True
 if DEBUG:
-    print("------")
     print("DEBUG mode is enabled")
     env_path = Path(".") / ".env.debug"
     load_dotenv(dotenv_path=env_path)
